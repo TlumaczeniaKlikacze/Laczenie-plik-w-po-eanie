b55699f0a78a072ccf1537404f211a6159c53e7a
TlumaczeniaKlikacze <89144762+TlumaczeniaKlikacze@users.noreply.github.com>
2021-10-19 09:04:18 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+const csv = require('csvtojson');
+const fs = require('fs')
+let list =[]
+let do_tego_dodac = []
+csv({delimiter:";",noheader:false})
+.fromFile("./files/dodac.csv")
+.subscribe((jsonObj,index)=>{
+    list.push(jsonObj)
+})
+.on('done',async()=>{
+    csv({delimiter:";",noheader:false})
+    .fromFile("./files/do_tego_dodac.csv")
+    .subscribe((jsonObj,index)=>{
+        do_tego_dodac.push(jsonObj)
+    })
+    .on('done',async()=>{
+        console.log(do_tego_dodac)
+        work()
+    })
+})
+let tmp = 0
+const csv_writer = require('csv-writer');
+const createCsvWrite = csv_writer.createObjectCsvWriter;
+const work = ()=>{
+    do_tego_dodac.forEach(el=>{
+       tmp = list.find(e=> e.ean == el.ean)
+       console.log(tmp)
+       if(tmp != undefined){
+           el.id = tmp.id
+       }
+       el.ean = '"'+el.ean+'"'
+
+    })
+    const csvWriter = createCsvWrite({fieldDelimiter:';', path:`pol.csv`,header:[
+        {id:"ean",title:"ean"},
+        {id:"brutto",title:"brutto"},
+        {id:"id",title:"id"},
+    
+    ]})
+    csvWriter
+    .writeRecords(do_tego_dodac)
+    .then(()=> console.log('done'))
+    .catch((er)=>{
+        console.log(er)
+    })
+}
